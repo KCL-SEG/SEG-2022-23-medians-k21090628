55ce24a0273790e5bff3814f821e24fc207b7bc4
k21090628 <114757710+k21090628@users.noreply.github.com>
2022-09-30 21:12:19 +0200

    Make output the median

--- median.py
+++ median.py
@@ -5,8 +5,16 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        numbers.sort()
+        if len(numbers) % 2 == 1:
+            median = numbers[int((len(numbers)-1)/2)]
+        else:
+            index = int(len(numbers)/2)
+            m1 = numbers[index - 1]
+            m2 = numbers[index]
+            median = (m1 + m2)/2
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print(median)
